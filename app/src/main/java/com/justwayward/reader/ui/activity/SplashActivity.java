eee96dc4446d05ce2b868265ac110ea4caa3a173
Meowzv <Meowzv@MeowzvdeMacBook-Pro.local>
2016-10-07 17:46:45 +0800

    修改首页，加入倒计时提示信息

--- app/src/main/java/com/justwayward/reader/ui/activity/SplashActivity.java
+++ app/src/main/java/com/justwayward/reader/ui/activity/SplashActivity.java
@@ -2,6 +2,8 @@ package com.justwayward.reader.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -18,6 +20,18 @@ public class SplashActivity extends AppCompatActivity {
 
     private boolean flag = false;
     private Runnable runnable;
+    private static int CURRENT_COUNT = 3;
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if(msg.what == 0){
+                if(CURRENT_COUNT > 0) {
+                    tvSkip.setText(--CURRENT_COUNT + " s");
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +46,27 @@ public class SplashActivity extends AppCompatActivity {
             }
         };
 
-        tvSkip.postDelayed(runnable, 2000);
-
         tvSkip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goHome();
             }
         });
+        tvSkip.postDelayed(runnable, CURRENT_COUNT*1000);
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                while (!flag){
+                    try {
+                        Thread.sleep(1000);
+                        mHandler.sendEmptyMessage(0);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }.start();
     }
 
     private synchronized void goHome() {
